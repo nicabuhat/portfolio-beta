85089d437364da7c5ad4bc70b25084c5f273ce9a
unknown <cabuhat.nicaela@gmail.com>
2019-09-09 14:42:22 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest, watch } = require("gulp");
+const scss = require("gulp-sass");
+sass.compiler = require("node-sass");
+
+/*
+ * directories
+ */
+let paths = {
+  src: "./src/",
+  scss: "./src/sass/",
+  css: "./build/assets/css/"
+};
+
+//compile sass
+function sass() {
+  return src(paths.scss + "*.scss")
+    .pipe(
+      scss({
+        includePaths: [paths.scss],
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(dest(paths.css));
+  // .pipe(browserSync.stream())
+}
+
+//watch sass
+exports.default = function() {
+  watch("src/sass/**/*.scss", sass);
+};
